8a084d9af4d40ea8daebb7a3a7e0b69c62113872
Thomas <thomascaycedo19@outlook.com>
2019-01-04 17:57:33 -0500

    Se guarda un juego a la base de datos local

--- server/build/controllers/gamesController.js
+++ server/build/controllers/gamesController.js
@@ -1,22 +1,38 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const database_1 = __importDefault(require("../routes/database"));
 class GamesController {
-    index(req, res) {
-        database_1.default.query('DESCRIBE games');
-        res.json('games');
+    list(req, res) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const games = yield database_1.default.query('SELECT * FROM games');
+            res.json(games);
+        });
+    }
+    getOne(req, res) {
+        res.json({ text: 'Listing One' + req.params.id });
     }
     create(req, res) {
-        res.json({ text: 'Create a game' });
+        return __awaiter(this, void 0, void 0, function* () {
+            yield database_1.default.query('INSERT INTO games set ?', [req.body]);
+            res.json({ message: 'Game Save' });
+        });
     }
     delete(req, res) {
-        res.json({ text: 'Deleting a game' });
+        res.json({ text: 'Deleting a game' + req.params.id });
     }
     update(req, res) {
-        res.json({ text: 'Updating a game' });
+        res.json({ text: 'Updating a game' + req.params.id });
     }
 }
 const gamesController = new GamesController();
--- server/src/controllers/gamesController.ts
+++ server/src/controllers/gamesController.ts
@@ -3,19 +3,23 @@ import pool from '../routes/database'
 
 class GamesController {
 
-    public index (req: Request, res: Response){
-        pool.query('DESCRIBE games')
-        res.json('games')
+    public async list (req: Request, res: Response){
+        const games = await pool.query('SELECT * FROM games');
+        res.json(games)
     } 
-    public create (req: Request, res:Response){
-        res.json({text: 'Create a game'})
+    public getOne(req: Request, res:Response){
+        res.json({text: 'Listing One'+ req.params.id})
+    }
+    public async create (req: Request, res:Response): Promise<void>{
+       await pool.query('INSERT INTO games set ?',[req.body])   
+        res.json({message: 'Game Save'})
     }
 
     public delete (req: Request, res:Response){
-        res.json({text: 'Deleting a game'})
+        res.json({text: 'Deleting a game'+ req.params.id})
     }
     public update(req: Request, res: Response){
-        res.json({text: 'Updating a game'})
+        res.json({text: 'Updating a game'+ req.params.id})
     }
  
 }
