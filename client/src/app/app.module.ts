5ff34af7df87552dac340d7a8a78baaf5afa0b64
Thomas <thomascaycedo19@outlook.com>
2019-01-04 22:57:33 -0500

    Se configura la ruta game

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+//Rutas
 import { AppRoutingModule } from './app-routing.module';
+
+//Componentes
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { GameFormComponent } from './components/game-form/game-form.component';
+import { GameListComponent } from './components/game-list/game-list.component';
+
+//Servicios
+import { GameService } from '../app/services/game.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    GameFormComponent,
+    GameListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    GameService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
